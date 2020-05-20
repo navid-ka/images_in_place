6d202ed6a08519d082141aa7c013c01c69e43aaa
Navid K-Bass <psychodelictrip@gmail.com>
2020-05-21 00:16:58 +0200

    another commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
 
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk
@@ -17,44 +17,30 @@ LabelBase.register(name='Nunito', fn_regular= 'assets/fonts/Nunito-Regular.ttf',
     fn_bold= 'assets/fonts/Nunito-Bold.ttf')
 
 
-class HBoxWidget(BoxLayout):
-    pass
+class VBoxWidget(BoxLayout):
 
-class VBoxWidget1(BoxLayout):
 
     def get_image_one(self):
         
         '''This method is called by button FileChooser, it opens a FileChooser selects the desired image'''
         Tk().withdraw()
-        img1 = askopenfilename(initialdir = "/",title = "Select file",filetypes = (("image files","*.jpg"),("all files","*.*")))
-        app = App.get_running_app()
-        app.first_image = img1
+        image = askopenfilename(initialdir = "\src\images",title = "Select file",filetypes = (("image files","*.jpg"),("all files","*.*")))  
+        self.filename.source = image
+    
+    def filter_one(self, filename):
         
-
-    def right_half_darker(self, first_image):
-
-        app = App.get_running_app()
-        image = SimpleImage(app.first_image)
+        image = SimpleImage(self.filename.source)
         for pixel in image:
             pixel.red = pixel.red * 1.5
             pixel.green = pixel.green * 0.7
             pixel.blue = pixel.blue * 1.5
-        app.first_image = image
 
-
-class VBoxWidget2(BoxLayout):
-    pass
-
-class ContainerBox(BoxLayout):
-    def __init__(self, **kwargs):
-        super(ContainerBox, self).__init__(**kwargs)
 
 class Main(App):
-    first_image = StringProperty('images/logo.png')
+    filename = StringProperty('images/logo.png')
     def build(self):
-        Window.clearcolor = (1, 1, 1, 1)
         Window.size = (1440 ,810)
-        return ContainerBox()
+        return VBoxWidget()
 
 if __name__ == '__main__':
     Main().run()
